b82e93060fa953c2fafed0aeb6d0d2eb88baafcf
Hector <thehectorr.05@gmail.com>
2024-12-06 06:53:52 -0400

    corrigiendo unos detallitos :)

--- Program.cs
+++ Program.cs
@@ -5,13 +5,12 @@ namespace Hospitaal
 {
 	class Program
 	{
-		public static void Main(string[] args)
+		public static void Main()
 		{
 			Console.WriteLine("Hola, Bienvenido al hospital Antonio Maria Pineda");
-			medico z = new medico();
-			Console.WriteLine
+			
 				
-			string integrantes = "Hector godoy y marcos castellanos" 
+			string integrantes = "Hector godoy y marcos castellanos";
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
